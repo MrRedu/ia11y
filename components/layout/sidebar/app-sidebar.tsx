46ecb761ed89e855680df17eb2139801ec935329
Eduardo <ejrb1234@gmail.com>
2026-01-18 17:56:48 -0400

    improved general styles

--- components/layout/sidebar/app-sidebar.tsx
+++ components/layout/sidebar/app-sidebar.tsx
@@ -16,14 +16,19 @@ import {
 import Link from 'next/link';
 import { FooterSidebar } from './footer-sidebar';
 import { useIntlayer } from 'next-intlayer';
+import { Ia11yIcon } from '@/components/atoms/icons/ia11y.icon';
 
 export function AppSidebar() {
   const content = useIntlayer('app-sidebar');
 
   return (
     <Sidebar collapsible="icon">
-      <SidebarHeader>
+      <SidebarHeader className="flex w-full flex-row items-center gap-2 pr-4">
         <SidebarTrigger />
+        <h1 className="font-mono font-bold flex items-center gap-2 ml-auto md:hidden">
+          <Ia11yIcon className="size-4 text-primary" aria-hidden="true" />
+          ia11y
+        </h1>
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
